c0468f0684f4dd5b90319c9f5513b178ba508ca7
Ivan Donato <donato.ivan@gmail.com>
2019-07-16 22:30:20 +1000

    preparing for deployment

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const connectDB = require('./config/db')
 const app = new express()
 const cors = require('cors');
+const path = require('path');
 
 require('dotenv').config()
 
 connectDB();
 
-const PORT = process.env.PORT || 5000;
 
 app.use(express.json({extended: false}))
 
@@ -20,6 +20,15 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client-side', 'build', 'index.html'))
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
